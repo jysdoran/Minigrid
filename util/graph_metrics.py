bugfix: prevent computation of impossible paths as solvable, improved validity checks, performance should also be a bit better

util/graph_metrics.py
@@ -5,6 +5,7 @@ import dgl
 import networkx as nx
 import numpy as np
 
+from data_generators import Batch
 
 def shortest_paths(graph:nx.Graph, source:int, target:int, num_paths:int=1) -> List[Any]:
     """
@@ -12,7 +13,7 @@ def shortest_paths(graph:nx.Graph, source:int, target:int, num_paths:int=1) -> L
     """
     assert num_paths >= 1
 
-    graph = nx.Graph(graph)
+    #graph = nx.Graph(graph)
 
     if not nx.has_path(graph, source, target):
         return []
@@ -26,32 +27,32 @@ def shortest_path_length(graph:nx.Graph, source:int, target:int) -> int:
     """
     Compute shortest path length from source to target in graph.
     """
-    graph = nx.Graph(graph)
+    #graph = nx.Graph(graph)
     return nx.shortest_path_length(graph, source, target)
     #return len(shortest_paths(graph, source, target, num_paths=1)[0])
 
 def resistance_distance(graph:nx.Graph, source:int, target:int) -> int:
     """
-    Compute resistance distance from source to target in graph.
+    Compute resistance distance from source to target in graph. Graph must be strongly connectected (1 single component)
     """
-    graph = nx.Graph(graph)
+    #graph = nx.Graph(graph)
     if not nx.has_path(graph, source, target):
         return np.nan
     else:
-        component = graph.subgraph(nx.node_connected_component(graph, source))
-        return nx.resistance_distance(component, source, target)
+       #component = graph.subgraph(nx.node_connected_component(graph, source))
+        return nx.resistance_distance(graph, source, target)
 
 def active_node_count(graph:nx.Graph) -> int:
     """
     Compute active node count in graph.
     """
-    graph = nx.Graph(graph)
+    #graph = nx.Graph(graph)
     graph.remove_nodes_from(list(nx.isolates(graph)))
     return nx.number_of_nodes(graph)
 
 def len_connected_component(graph:nx.Graph, source, target) -> int:
 
-    graph = nx.Graph(graph)
+    #graph = nx.Graph(graph)
     if not nx.has_path(graph, source, target):
         return np.nan
 
@@ -63,24 +64,52 @@ def is_solvable(graph:nx.Graph, source, target) -> bool:
     """
     return nx.has_path(graph, source, target)
 
-def is_valid(graph:nx.Graph, start, goal) -> bool:
+def valid_start_goal(start_nodes, goal_nodes, A_red):
+    """
+    Check if the start and goal nodes are valid.
+    """
+
+    batch_inds = torch.arange(0, start_nodes.shape[0])
+
+    mask1 = start_nodes == goal_nodes
+    mask2 = A_red[batch_inds, start_nodes].sum(dim=-1) == 0
+    mask3 = A_red[batch_inds, goal_nodes].sum(dim=-1) == 0
+    # valid only if NOT(start==goal AND no edges from start AND no edges from goal)
+    valid = ~(mask1 & mask2 & mask3)
+    #TODO: check with this
+    # check that start and goal are not isolated (inactive) nodes
+    #if start in list(nx.isolates(graph)) or goal in list(nx.isolates(graph)):
+        #return False
+
+    return valid
+
+def check_validity_and_convert_to_graph(mode_A, mode_Fx, start_dim, goal_dim, check_validity=True, correct_A=False, correct_Fx=False):
     """
     Check if the graph is valid.
     """
 
-    # check start and goal nodes are not placed in the same locaiton
-    if start == goal:
-        return False
+    #TODO correct_Fx
+    mode_A = mode_A.reshape(mode_A.shape[0], -1, 2)
+    n_nodes = mode_Fx.shape[1]
+
+    start_nodes = mode_Fx[...,start_dim].argmax(dim=-1)
+    goal_nodes = mode_Fx[...,goal_dim].argmax(dim=-1)
+
+    if check_validity:
+        mode_A, valid_A = Batch.is_valid_reduced_A(mode_A, n_nodes, correct_A)
+        valid_start_goal = valid_start_goal(start_nodes, goal_nodes, mode_A)
+        valid = valid_A & valid_start_goal
+    else:
+        valid = None
 
 
-    # check that start and goal are not isolated (inactive) nodes
-    if start in list(nx.isolates(graph)) or goal in list(nx.isolates(graph)):
-        return False
+    #TODO sort out force_valid
+    graphs = Batch.encode_decoder_mode_to_graph(mode_A, mode_Fx, force_valid_A=False, device=mode_A.device)
 
-    return True
+    return graphs, start_nodes, goal_nodes, valid
 
-def compute_metrics(graphs:List[dgl.DGLGraph], desired_metrics:List[str]=
-    ["valid","solvable","shortest_path", "resistance", "navigable_nodes"], start_dim=2, goal_dim=3):
+def compute_metrics(mode_A, mode_Fx, desired_metrics:List[str]=
+    ["valid","solvable","shortest_path", "resistance", "navigable_nodes"], start_dim=2, goal_dim=3) -> dict:
     """
     :param graphs: List[DGLGraph]
     :param desired_metrics: List[str]
@@ -89,29 +118,36 @@ def compute_metrics(graphs:List[dgl.DGLGraph], desired_metrics:List[str]=
     :return: metrics: Dict[str, List[float]]
     """
 
+    graphs, start_nodes, goal_nodes, valid_mask = check_validity_and_convert_to_graph(mode_A, mode_Fx, start_dim, goal_dim)
+
     metrics = {k: [] for k in desired_metrics}
+    try:
+        metrics["valid"] = valid_mask.tolist()
+    except KeyError:
+        pass
 
-    for graph in graphs:
+    for i, graph in enumerate(graphs):
         if isinstance(graph, dgl.DGLGraph):
-            start_node = int(graph.ndata["feat"][:, start_dim].argmax())
-            goal_node = int(graph.ndata["feat"][:, goal_dim].argmax())
             graph = graph.to_networkx()
+            graph = nx.Graph(graph)
         else:
-            raise ValueError("graph must be either a DGLGraph or a nx.Graph")
+            raise ValueError("graph must be a DGLGraph")
 
-        solvable = valid = False
+        solvable = False
         computed_m = []
 
         for metric in desired_metrics:
-            if metric == "valid":
-                valid = is_valid(graph, start_node, goal_node)
-                metrics[metric].append(valid)
-                computed_m.append(metric)
-            elif metric == "solvable":
-                if "valid" in computed_m and not valid:
-                    solvable = False
-                else:
+            if metric == "solvable":
+                try:
+                    valid = metrics["valid"][i]
+                except KeyError:
+                    valid = True
+                if valid:
+                    start_node = start_nodes[i]
+                    goal_node = goal_nodes[i]
                     solvable = is_solvable(graph, start_node, goal_node)
+                else:
+                    solvable = False
                 metrics[metric].append(solvable)
                 computed_m.append(metric)
             else:
@@ -122,6 +158,7 @@ def compute_metrics(graphs:List[dgl.DGLGraph], desired_metrics:List[str]=
                     computed_m.extend(["shortest_path", "resistance", "navigable_nodes"])
                     break
                 else:
+                    graph = graph.subgraph(nx.node_connected_component(graph, start_node))
                     if metric == "shortest_path":
                         metrics[metric].append(shortest_path_length(graph, start_node, goal_node))
                         computed_m.append(metric)

util/logger_callbacks.py
@@ -3,13 +3,12 @@ import logging
 import dgl
 import networkx as nx
 import numpy as np
-from typing import List, Union, Tuple, Optional, Any
+from typing import List, Union, Tuple, Optional, Any, Dict
 import torch
 import pytorch_lightning as pl
 import wandb
 import einops
 import pandas as pd
-from typing import Dict
 
 from data_generators import Batch, OBJECT_TO_CHANNEL_AND_IDX
 encode_graph_to_gridworld = Batch.encode_graph_to_gridworld
@@ -22,10 +21,20 @@ from copy import deepcopy
 logger = logging.getLogger(__name__)
 
 class GraphVAELogger(pl.Callback):
-    def __init__(self, label_contents:Dict, samples, attributes, used_attributes, num_samples=32, max_cached_batches=0, accelerator="cpu"):
+    def __init__(self,
+                 label_contents: Dict[str, Any],
+                 samples: Dict[str, Tuple[dgl.DGLGraph, torch.Tensor]],
+                 attributes: List[str],
+                 used_attributes: List[str],
+                 label_descriptors_config: Dict = None,
+                 num_samples: int = 32,
+                 max_cached_batches: int = 0,
+                 accelerator: str = "cpu"):
+
         super().__init__()
         device = torch.device("cuda" if accelerator == "gpu" else "cpu")
         self.label_contents = label_contents
+        self.label_descriptors_config = label_descriptors_config
         self.num_samples = num_samples
         self.max_cached_batches = max_cached_batches
         self.attributes = attributes
@@ -252,6 +261,9 @@ class GraphVAELogger(pl.Callback):
             start_dim=pl_module.decoder.attributes.index("start"),
             goal_dim=pl_module.decoder.attributes.index("goal"))
 
+        if self.label_descriptors_config is not None:
+            reconstruction_metrics = self.normalise_metrics(reconstruction_metrics, device=pl_module.device)
+
         reconstructed_imgs = self.obtain_imgs(logits_A, logits_Fx, pl_module)
 
         del logits_A, logits_Fx, mode_probs_A, mode_probs_Fx, reconstructed_graphs
@@ -358,3 +370,15 @@ class GraphVAELogger(pl.Callback):
             imgs[cm>0] = cm[cm>0]
 
         return imgs
+
+    def normalise_metrics(self, metrics, device=torch.device("cpu")):
+
+        for key in metrics.keys():
+            if key in self.label_descriptors_config.keys():
+                if isinstance(metrics[key], list):
+                    metrics[key] = torch.tensor(metrics[key]).to(device, torch.float)
+                metrics[key] /= self.label_descriptors_config[key].normalisation_factor
+                if isinstance(metrics[key], torch.Tensor):
+                    metrics[key] = metrics[key].tolist()
+
+        return metrics