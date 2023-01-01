84f454bcb224f08f88533a28a1839c56e9e89afb
francelico <garcin.samuel@gmail.com>
2023-01-01 23:00:17 +0000

    updated run_slurm

--- run_slurm.sh
+++ run_slurm.sh
@@ -6,11 +6,11 @@
 #SBATCH --cpus-per-task=16
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate curri
+conda activate dcd2
 #export PYTHONPATH=$HOME/
-cd $HOME/auto-curriculum-design/maze_representations/
+cd $HOME/auto-curriculum-design/_dcd/
 export WANDB_START_METHOD=thread
 #wandb offline
 #export WANDB_MODE=offline
 # export WANDB_API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-/home/sgar/miniconda3/envs/curri/bin/python run_experiments.py -m run_name=new_interpolation +sweeper=wandb_VAE_10k +launcher=submitit_remote num_cpus=16 offline=false
+$HOME/miniconda3/envs/dcd2/bin/python3.8 maze_representations/run_experiments.py -m run_name=minigrid_dense_graph_22k accelerator=gpu results.num_stored_samples=256 results.num_image_samples=32 results.num_embedding_samples=256 results.num_generated_samples=32 results.sample_interpolation.num_samples=32 +sweeper=wandb_VAE_densegraph hydra.job.chdir=False +launcher=submitit_remote num_cpus=6 offline=false
